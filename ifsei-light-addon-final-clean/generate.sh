33a2b1f47cdfbdea924c9cdf758b550523e6b024
mrsilberberg <161399872+mrsilberberg@users.noreply.github.com>
2025-08-15 15:44:20 -0300

    Update generate.sh

--- ifsei-light-addon-final-clean/generate.sh
+++ ifsei-light-addon-final-clean/generate.sh
@@ -23,8 +23,7 @@ shell_command:
 
   ifsei_set: >-
       bash -c "pkill -9 nc 2>/dev/null || true;
-      echo -ne '\$D{{ mod }}Z{{ zone }}{{ ("%02d") % ((brightness | int * 63 // 255)) }}T0\r' |
-      nc -w1 $IP $PORT"
+      echo -ne '\$D{{ mod }}Z{{ zone }}{{ ("%02d") % ((brightness | int * 63 // 255)) }}T0\r' | nc -w1 $IP $PORT"
 
 input_text:
 EOF
