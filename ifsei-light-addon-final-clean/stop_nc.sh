f4c06700e690c4ba9a44b618c213bcbdd99b55d1
mrsilberberg <161399872+mrsilberberg@users.noreply.github.com>
2025-08-15 21:40:32 -0300

    Update stop_nc.sh

--- ifsei-light-addon-final-clean/stop_nc.sh
+++ ifsei-light-addon-final-clean/stop_nc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Mata todas as conexões netcat no container
 pkill -9 nc 2>/dev/null || true
-killall nc
-killall -q nc
+killall nc 2>/dev/null || true
+killall -q nc 2>/dev/null || true
 
