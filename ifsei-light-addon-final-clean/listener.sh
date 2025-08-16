fac9ffaf9236a0d62346d48989ca679448af8c7d
mrsilberberg <161399872+mrsilberberg@users.noreply.github.com>
2025-08-15 22:24:48 -0300

    Update listener.sh

--- ifsei-light-addon-final-clean/listener.sh
+++ ifsei-light-addon-final-clean/listener.sh
@@ -12,7 +12,7 @@ echo $$ > "$PID_FILE"  # Guarda o PID do próprio processo
 
 while true; do
   echo "[INFO] Conectando..."
-  nc "$IP" "$PORT" | tee -a "$LOG_FILE"
+  nc -w86400 "$IP" "$PORT" | tee -a "$LOG_FILE"
   echo "[WARN] Conexão perdida. Tentando reconectar..."
   sleep 1
 done
